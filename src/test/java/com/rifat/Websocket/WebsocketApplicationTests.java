c6db3561384623826f5ad9bca3777b18d834fb33
Sahariar Alam Khandoker Rifat <sahariar.alam@therapservices.net>
2023-02-05 19:46:13 +0600

    Initialized project

---
+++ src/test/java/com/rifat/Websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifat.Websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
